475eb745314f034bc2a95e959388885a93d4a7e4
Kami <a1864343@adelaide.edu.au>
2025-06-20 12:10:03 +0000

    [automatic commit] 2025-06-20 12:10:03 UTC 121.44.212.34

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -10,7 +10,9 @@ router.get('/owners-dogs', async (req, res) => {
     }
 
     try {
-        const [rows] = await db.query
+        const [rows] = await db.query(`
+            SELECT dog_id, name FROM Dogs WHERE owner_id = ?
+            `)
     }
 });
 
