ed23931184418e0b25612af5f2284bc29e1cb6e5
Kami <a1864343@adelaide.edu.au>
2025-06-20 11:53:50 +0000

    [automatic commit] 2025-06-20 11:53:50 UTC 121.44.212.34

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -3,4 +3,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
+// GET all the dogs for the logged in owner
+
+
 module.exports = router;
