992b037cb75f5a81d3705ec1196935cdb8558e39
Kami <a1864343@adelaide.edu.au>
2025-06-20 05:41:00 +0000

    [automatic commit] 2025-06-20 05:40:59 UTC 121.44.212.34

--- part1/app.js
+++ part1/app.js
@@ -81,8 +81,19 @@ app.get('/api/dogs', async (req, res) => {
     }
 });
 
-// api.walrequests/open
-
+// api.walkrequests/open
+app.get('/api/dogs', async (req, res) => {
+    try {
+        const [rows] = await db.execute(`
+            SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
+            FROM Dogs JOIN Users ON Dogs.owner_id = Users.user_id
+            `);
+            res.json(rows);
+    } catch (err) {
+        console.error('Sorry! Error found in the api/dogs route: ', err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
