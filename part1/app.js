b4f8c4512a037ba39a788c9bf3d6ed839860799d
Kami <a1864343@adelaide.edu.au>
2025-06-20 04:57:20 +0000

    [automatic commit] 2025-06-20 04:57:20 UTC 121.44.212.34

--- part1/app.js
+++ part1/app.js
@@ -28,11 +28,12 @@ let db;
     await db.execute(`
         INSERT INTO Users (username, email, password_hash, role) VALUES
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'), 
-    -> ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-    -> ('torivega', 'toriv@example.com', 'hashed987', 'walker'), 
-    -> ('trinavega', 'trinav@example.com', 'hashed321', 'owner');
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('torivega', 'toriv@example.com', 'hashed987', 'walker'),
+        ('trinavega', 'trinav@example.com', 'hashed321', 'owner');
         `)
+        
     await db.execute(`
         INSERT IGNORE INTO Dogs(owner_id, name, size) VALUES
             ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
