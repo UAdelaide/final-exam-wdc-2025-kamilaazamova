bd748dd519e147ec884f9a66f474b81c404de7fa
Kami <a1864343@adelaide.edu.au>
2025-06-20 12:10:24 +0000

    [automatic commit] 2025-06-20 12:10:24 UTC 121.44.212.34

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -12,7 +12,7 @@ router.get('/owners-dogs', async (req, res) => {
     try {
         const [rows] = await db.query(`
             SELECT dog_id, name FROM Dogs WHERE owner_id = ?
-            `)
+            `), [req.session.user.]
     }
 });
 
