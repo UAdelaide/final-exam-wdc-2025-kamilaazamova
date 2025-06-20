cc28e9d9375ed697c22678df675137072958dabd
Kami <a1864343@adelaide.edu.au>
2025-06-20 04:58:08 +0000

    [automatic commit] 2025-06-20 04:58:07 UTC 121.44.212.34

--- part1/app.js
+++ part1/app.js
@@ -24,7 +24,7 @@ let db;
     });
 
     // now we insert test data for api endpoints
-    // first three taken from previous question
+    // first three taken from that previous question
     // then need to complete other 2 tables
     await db.execute(`
         INSERT INTO Users (username, email, password_hash, role) VALUES
@@ -33,7 +33,7 @@ let db;
         ('carol123', 'carol@example.com', 'hashed789', 'owner'),
         ('torivega', 'toriv@example.com', 'hashed987', 'walker'),
         ('trinavega', 'trinav@example.com', 'hashed321', 'owner');
-        `)
+        `);
 
     await db.execute(`
         INSERT IGNORE INTO Dogs(owner_id, name, size) VALUES
@@ -41,8 +41,10 @@ let db;
             ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
             ((SELECT user_id FROM Users WHERE username = 'trinavega'), 'Maurice', 'large'),
             ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Julian', 'small'),
-            ((SELECT user_id FROM Users WHERE username = 'trinavega'), 'Smudge', 'medium');`
-    )
+            ((SELECT user_id FROM Users WHERE username = 'trinavega'), 'Smudge', 'medium');
+        `);
+
+
 
 
 app.use(express.static(path.join(__dirname, 'public')));
