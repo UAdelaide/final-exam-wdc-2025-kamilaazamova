38c248b71c59728eeeba12864b576dd06c15e5c7
Kami <a1864343@adelaide.edu.au>
2025-06-20 12:20:56 +0000

    [automatic commit] 2025-06-20 12:20:56 UTC 121.44.212.34

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -87,4 +87,22 @@ router.get('/logout', (req, res) => {
   });
 });
 
+// GET all the dogs for the logged in owner
+router.get('/my-dogs', async (req, res) => {
+    if (!req.session.user || req.session.user.role !== 'owner') {
+        return res.status(401).json({ error: 'Wrong user role'});
+    }
+
+    try {
+        const [rows] = await db.query(`
+            SELECT dog_id, name FROM Dogs WHERE owner_id = ?
+            `, [req.session.user.owner_id]);
+
+            res.json(rows);
+    } catch (err) {
+        console.error('Failed to fetch dogs: ', err);
+        res.status(500).json({ error: 'server error'});
+    }
+});
+
 module.exports = router;
