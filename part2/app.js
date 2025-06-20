b52efec3afd6ab2a1527227dbea4e8d228d32e56
Kami <a1864343@adelaide.edu.au>
2025-06-20 13:26:33 +0000

    [automatic commit] 2025-06-20 13:26:33 UTC 121.44.212.34

--- part2/app.js
+++ part2/app.js
@@ -25,5 +25,18 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
+app.get('/api/dogs', async (req, res) => {
+    try {
+        const [rows] = await db.execute(`
+            SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
+            FROM Dogs JOIN Users ON Dogs.owner_id = Users.user_id
+            `);
+            res.json(rows);
+    } catch (err) {
+        console.error('Sorry! Error found in the api/dogs route: ', err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
 // Export the app instead of listening here
 module.exports = app;
