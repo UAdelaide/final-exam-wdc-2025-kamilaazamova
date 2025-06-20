aa02bd6b23d45e9fdb784dd56d3d9376e0194845
Kami <a1864343@adelaide.edu.au>
2025-06-20 05:06:06 +0000

    [automatic commit] 2025-06-20 05:06:06 UTC 121.44.212.34

--- part1/app.js
+++ part1/app.js
@@ -57,7 +57,9 @@ let db;
         INSERT IGNORE  WalkRatings (walker_id, rating) VALUES
             ((SELECT user_id FROM Users WHERE username = 'bobwalker'), 4),
             ((SELECT user_id FROM Users WHERE username = 'bobwalker'), 5),
-        `)
+        `);
+
+    
 
 
 app.use(express.static(path.join(__dirname, 'public')));
