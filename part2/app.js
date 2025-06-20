e482c0f218b9d21c87297dc21e678c9db07c293a
Kami <a1864343@adelaide.edu.au>
2025-06-20 10:00:57 +0000

    [automatic commit] 2025-06-20 10:00:56 UTC 121.44.212.34

--- part2/app.js
+++ part2/app.js
@@ -11,7 +11,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(bodyParser.urlencoded({ entended: true}));
-app.use
+app.use(session({
+    secret: ''
+}))
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
